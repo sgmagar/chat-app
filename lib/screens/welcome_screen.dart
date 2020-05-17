059a9919add61b2ccacd1699e4bea3d9d90d029f
Saurav <sp.gharti@gmail.com>
2020-05-17 09:11:05 +0545

    add named routes

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -42,7 +42,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, 'login_screen');
                   },
                   minWidth: 200.0,
                   height: 42.0,
